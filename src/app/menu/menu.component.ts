6639d2a3f9b7a6968e224626129e1224ac8a2707
Artur Dolzan Neto <arturdolzan@gmail.com>
2018-11-27 13:25:20 -0200

    opa

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  templateUrl: './menu.component.html']
+  templateUrl: './menu.component.html'
 })
 export class MenuComponent implements OnInit {
 
